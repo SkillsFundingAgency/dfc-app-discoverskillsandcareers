07ef733821acd1454faf93ac5e75d1a3e530d03a
Ilyas Dhin <ilyas.dhin@contractsonline.org>
2020-01-31 09:06:07 +0000

    Added a head endpoint for consumption by composite

---
+++ DFC.App.DiscoverSkillsCareers/Controllers/CompositeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DFC.App.DiscoverSkillsCareers.Controllers
+{
+    public class CompositeController : BaseController
+    {
+        [Route("head")]
+        public IActionResult Head()
+        {
+            return View();
+        }
+    }
+}
