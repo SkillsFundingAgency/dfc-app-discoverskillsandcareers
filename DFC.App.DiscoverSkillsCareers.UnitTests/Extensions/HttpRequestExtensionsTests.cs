be935ae78937fa35dfca6b5ca5c50ee4f38d9ffe
Ilyas Dhin <ilyas.dhin@contractsonline.org>
2020-02-04 09:43:08 +0000

    Added unit tests

---
+++ DFC.App.DiscoverSkillsCareers.UnitTests/Extensions/HttpRequestExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using DFC.App.DiscoverSkillsCareers.Extensions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Xunit;
+
+namespace DFC.App.DiscoverSkillsCareers.UnitTests.Extensions
+{
+    public class HttpRequestExtensionsTests
+    {
+        [Fact]
+        public void IsRequestFromCompositeReturnsTrueWhenContainsCompositeRequestHeader()
+        {
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = httpContextAccessor.Object.HttpContext = new DefaultHttpContext();
+
+            var request = httpContext.Request;
+            request.Headers.Add("X-Dfc-Composite-Request", "somevalue");
+
+            Assert.True(request.IsRequestFromComposite());
+        }
+
+        [Fact]
+        public void IsRequestFromCompositeReturnsFalseWhenContainsNoCompositeRequestHeader()
+        {
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = httpContextAccessor.Object.HttpContext = new DefaultHttpContext();
+
+            var request = httpContext.Request;
+
+            Assert.False(request.IsRequestFromComposite());
+        }
+    }
+}
