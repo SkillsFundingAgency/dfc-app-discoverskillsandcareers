303a8960a9428d17d84969cf909bfef6b4f13554
Ilyas Dhin <ilyas.dhin@contractsonline.org>
2020-02-07 13:47:21 +0000

    Removed json attributes

--- DFC.App.DiscoverSkillsCareers.Models/QuestionSet.cs
+++ DFC.App.DiscoverSkillsCareers.Models/QuestionSet.cs
@@ -5,34 +5,25 @@ namespace DFC.App.DiscoverSkillsCareers.Models
 {
     public class QuestionSet
     {
-        [JsonProperty("partitionKey")]
         public string PartitionKey { get; set; }
 
         [JsonProperty("id")]
         public string QuestionSetVersion { get; set; }
 
-        [JsonProperty("version")]
         public int Version { get; set; }
 
-        [JsonProperty("assessmentType")]
         public string AssessmentType { get; set; }
 
-        [JsonProperty("maxQuestions")]
         public int MaxQuestions { get; set; }
 
-        [JsonProperty("lastUpdated")]
         public DateTimeOffset LastUpdated { get; set; }
 
-        [JsonProperty("description")]
         public string Description { get; set; }
 
-        [JsonProperty("title")]
         public string Title { get; set; }
 
-        [JsonProperty("titleLowercase")]
-        public string QuestionSetKey { get; set; }
+        public string TitleLowercase { get; set; }
 
-        [JsonProperty("isCurrent")]
         public bool IsCurrent { get; set; }
     }
 }
