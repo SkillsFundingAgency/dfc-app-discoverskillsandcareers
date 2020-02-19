9853af1ff507b5971a4ad856c6fe78f50de37597
Ilyas Dhin <ilyas.dhin@contractsonline.org>
2020-02-19 09:54:20 +0000

    Added new test project and removed old one

---
+++ DFC.App.DiscoverSkillsCareers.Services.UnitTests/SessionIdToCodeConvertersTests/DefaultISessionIdToCodeConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using DFC.App.DiscoverSkillsCareers.Services.SessionIdToCodeConverters;
+using Xunit;
+
+namespace DFC.App.DiscoverSkillsCareers.Services.UnitTests.SessionIdToCodeConvertersTests
+{
+    public class DefaultISessionIdToCodeConverterTests
+    {
+        private readonly SessionIdToCodeConverter defaultSessionIdToCodeConverter;
+
+        public DefaultISessionIdToCodeConverterTests()
+        {
+            defaultSessionIdToCodeConverter = new SessionIdToCodeConverter();
+        }
+
+        [Theory]
+        [InlineData("e", "E")]
+        [InlineData("ez", "EZ")]
+        [InlineData("ezw", "EZW")]
+        [InlineData("ezw6", "EZW6")]
+        [InlineData("ezw6 ", "EZW6")]
+        [InlineData("ezw68", "EZW6 8")]
+        [InlineData("ezw689", "EZW6 89")]
+        [InlineData("ezw689 ", "EZW6 89")]
+        [InlineData("ezw68mmyw943m3", "EZW6 8MMY W943 M3")]
+        public void CanGetCode(string source, string expectedCode)
+        {
+            var actualCode=defaultSessionIdToCodeConverter.GetCode(source);
+            Assert.Equal(expectedCode, actualCode);
+        }
+
+        [Theory]
+        [InlineData("EZW6 8MMY W943 M3", "ezw68mmyw943m3")]
+        [InlineData("EZW6 8MMY W943 m3", "ezw68mmyw943m3")]
+        [InlineData(" EZW6 8MMY W943 m3 ", "ezw68mmyw943m3")]
+        public void CanGetSessionId(string source, string expectedSessionId)
+        {
+            var actualSessionId = defaultSessionIdToCodeConverter.GetSessionId(source);
+            Assert.Equal(expectedSessionId, actualSessionId);
+        }
+    }
+}
