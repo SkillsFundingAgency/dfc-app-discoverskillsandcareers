00f78668071a0e3e3f33c71ed8ad2f1b6183bf37
Ilyas Dhin <ilyas.dhin@contractsonline.org>
2020-03-03 13:45:42 +0000

    Updated app settings for api's

---
+++ DFC.App.DiscoverSkillsCareers.Models/Common/HttpClientSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.App.DiscoverSkillsCareers.Models.Common
+{
+    [ExcludeFromCodeCoverage]
+    public class HttpClientSettings
+    {
+        public TimeSpan Timeout { get; set; }
+
+        public Uri BaseAddress { get; set; }
+
+        public string OcpApimSubscriptionKey { get; set; }
+    }
+}
