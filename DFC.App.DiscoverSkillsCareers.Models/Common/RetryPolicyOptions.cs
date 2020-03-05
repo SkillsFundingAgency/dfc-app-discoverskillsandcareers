a4548839969eff46561fb74b92692f62118d74ea
gurney <Gurney@as-tech.co.uk>
2020-03-05 11:10:38 +0000

    Added JPOverview client

---
+++ DFC.App.DiscoverSkillsCareers.Models/Common/RetryPolicyOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.App.DiscoverSkillsCareers.Models.Common
+{
+    [ExcludeFromCodeCoverage] // This model is only used by startup in polly setup extention methods and hence can not be used in tests.
+    public class RetryPolicyOptions
+    {
+        public int Count { get; set; } = 3;
+
+        public int BackoffPower { get; set; } = 2;
+    }
+}
--- DFC.App.DiscoverSkillsCareers.Services.UnitTests/ServiceTests/ResultsApiServiceTests.cs
+++ DFC.App.DiscoverSkillsCareers.Services.UnitTests/ServiceTests/ResultsApiServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using DFC.App.DiscoverSkillsCareers.Services.Api;
 using DFC.App.DiscoverSkillsCareers.Services.Contracts;
 using DFC.App.DiscoverSkillsCareers.Services.Serialisation;
+using FakeItEasy;
 using RichardSzalay.MockHttp;
 using System;
 using System.Net.Http;
@@ -15,16 +16,18 @@ namespace DFC.App.DiscoverSkillsCareers.Services.UnitTests.ServiceTests
         private readonly MockHttpMessageHandler httpMessageHandler;
         private readonly HttpClient httpClient;
         private readonly ISerialiser serialiser;
+        private readonly IJpOverviewApiService fakeJpOverviewApiService;
 
         public ResultsApiServiceTests()
         {
             serialiser = new NewtonsoftSerialiser();
+            fakeJpOverviewApiService = A.Fake<IJpOverviewApiService>();
 
             httpMessageHandler = new MockHttpMessageHandler();
             httpClient = httpMessageHandler.ToHttpClient();
             httpClient.BaseAddress = new Uri("https://localhost/resultsapi");
 
-            resultsApiService = new ResultsApiService(httpClient, serialiser);
+            resultsApiService = new ResultsApiService(httpClient, serialiser, fakeJpOverviewApiService);
         }
 
         [Fact]
