7d2269f4f88557404d447d2009ffcf16e2ddecf2
Ilyas Dhin <ilyas.dhin@contractsonline.org>
2020-03-09 13:41:35 +0000

    Added more coverage

--- DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.Assessment/ReferenceGetTests.cs
+++ DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.Assessment/ReferenceGetTests.cs
@@ -21,5 +21,24 @@ namespace DFC.App.DiscoverSkillsCareers.UnitTests.Controllers.Assessment
             var redirectResult = actionResponse as RedirectResult;
             Assert.Equal($"~/{RouteName.Prefix}/", redirectResult.Url);
         }
+
+        [Fact]
+        public async Task ReferenceReturnsView()
+        {
+            var sessionId = "session1";
+            A.CallTo(() => SessionClient.TryFindSessionCode()).Returns(sessionId);
+
+            var actionResponse = await AssessmentController.Reference().ConfigureAwait(false);
+
+            Assert.IsType<ViewResult>(actionResponse);
+        }
+
+        [Fact]
+        public void ReferenceSentReturnsView()
+        {
+            var actionResponse = AssessmentController.ReferenceSent();
+
+            Assert.IsType<ViewResult>(actionResponse);
+        }
     }
 }
