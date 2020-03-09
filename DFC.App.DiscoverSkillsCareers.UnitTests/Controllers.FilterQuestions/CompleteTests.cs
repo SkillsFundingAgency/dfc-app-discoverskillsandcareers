89d40ad41f88e7e434bc3d46b80d291dbc60c942
Ilyas Dhin <ilyas.dhin@contractsonline.org>
2020-03-09 13:48:50 +0000

    Added more coverage

--- DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.FilterQuestions/CompleteTests.cs
+++ DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.FilterQuestions/CompleteTests.cs
@@ -31,14 +31,26 @@ namespace DFC.App.DiscoverSkillsCareers.UnitTests.Controllers.FilterQuestions
         public async Task WhenNoSessionIdRedirectsToRoot()
         {
             string sessionId = null;
-            var viewModel = new FilterQuestionIndexRequestViewModel();
+            var viewModel = new FilterQuestionsCompleteResponseViewModel();
             A.CallTo(() => sessionClient.TryFindSessionCode()).Returns(sessionId);
 
-            var actionResponse = await controller.Index(viewModel).ConfigureAwait(false);
+            var actionResponse = await controller.Complete(viewModel).ConfigureAwait(false);
 
             Assert.IsType<RedirectResult>(actionResponse);
             var redirectResult = actionResponse as RedirectResult;
             Assert.Equal($"~/{RouteName.Prefix}/", redirectResult.Url);
+        }
+
+        [Fact]
+        public async Task WhenSessionIdExistsReturnsView()
+        {
+            var sessionId = "sessionId1";
+            var viewModel = new FilterQuestionsCompleteResponseViewModel();
+            A.CallTo(() => sessionClient.TryFindSessionCode()).Returns(sessionId);
+
+            var actionResponse = await controller.Complete(viewModel).ConfigureAwait(false);
+
+            Assert.IsType<ViewResult>(actionResponse);
         }
     }
 }
