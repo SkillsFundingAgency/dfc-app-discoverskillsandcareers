461ae78f99a1921adc170a839b8dc110fd6b65e9
Sumair Ahmed <snipered@hotmail.com>
2020-05-17 02:16:46 +0100

    wip

---
+++ DFC.App.DiscoverSkillsCareers/Validation/ReferenceCodeExists.cs
@@ -0,0 +1,16 @@
+﻿using DFC.App.DiscoverSkillsCareers.Services.Contracts;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace DFC.App.DiscoverSkillsCareers.Validation
+{
+    public class ReferenceCodeExists : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var assessmentService = DependencyResolver.Current.GetService<IAssessmentService>();
+
+            return assessmentService.ReferenceCodeExists(value.ToString());
+        }
+    }
+}
--- DFC.App.DiscoverSkillsCareers/ViewModels/Home/HomeIndexResponseViewModel.cs
+++ DFC.App.DiscoverSkillsCareers/ViewModels/Home/HomeIndexResponseViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DFC.App.DiscoverSkillsCareers.Validation;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace DFC.App.DiscoverSkillsCareers.ViewModels
 {
     public class HomeIndexResponseViewModel
     {
         [Display(Name = "Reference Code")]
-        [Required]
+        [Required(ErrorMessage = "Enter your reference")]
+        [ReferenceCodeExists(ErrorMessage = "The reference could not be found")]
         public string ReferenceCode { get; set; }
     }
 }
