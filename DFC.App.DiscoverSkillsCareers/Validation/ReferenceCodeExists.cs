cffd8fef1dfc12c7211bec6acd738ff5a150ccdf
Sumair Ahmed <snipered@hotmail.com>
2020-05-18 15:18:35 +0100

    correction for di

--- DFC.App.DiscoverSkillsCareers/Validation/ReferenceCodeExists.cs
+++ DFC.App.DiscoverSkillsCareers/Validation/ReferenceCodeExists.cs
@@ -1,16 +1,32 @@
 ﻿using DFC.App.DiscoverSkillsCareers.Services.Contracts;
 using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace DFC.App.DiscoverSkillsCareers.Validation
 {
     public class ReferenceCodeExists : ValidationAttribute
     {
+        private readonly HttpContext _httpContext;
+
+        public ReferenceCodeExists(IHttpContextAccessor contextAccessor)
+        {
+            _httpContext = contextAccessor.HttpContext;
+        }
+
+        public ReferenceCodeExists()
+        {
+            
+        }
+
         public override bool IsValid(object value)
         {
-            var assessmentService = DependencyResolver.Current.GetService<IAssessmentService>();
+            var services = this._httpContext.RequestServices;
+            var assessment = (IAssessmentService)services.GetService(typeof(IAssessmentService));
 
-            return assessmentService.ReferenceCodeExists(value.ToString());
+            return assessment.ReferenceCodeExists(value.ToString());
         }
     }
 }
