ff1476089fccf9c0e491e2fe20cc0cf5aa3e2d7d
Tom Tollafield <thomas.tollafield@gmail.com>
2020-09-30 07:14:29 +0100

    - Remove using statement

--- DFC.App.DiscoverSkillsCareers.Services/Services/WebhooksService.cs
+++ DFC.App.DiscoverSkillsCareers.Services/Services/WebhooksService.cs
@@ -1,5 +1,4 @@
-﻿using DFC.App.DiscoverSkillsCareers.Models.API;
-using DFC.App.DiscoverSkillsCareers.Models.Contracts;
+﻿using DFC.App.DiscoverSkillsCareers.Models.Contracts;
 using DFC.App.DiscoverSkillsCareers.Models.Enums;
 using DFC.App.DiscoverSkillsCareers.Services.Contracts;
 using DFC.App.DiscoverSkillsCareers.Services.Helpers;
