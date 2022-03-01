9184983b09298596b9604aa22125a7722b383223
LAPTOP-CTLLJBUM\man_l <man_luck@yahoo.com>
2022-03-01 08:54:52 +0000

    Clean up

--- DFC.App.DiscoverSkillsCareers.TestSuite/EnvironmentSettings.cs
+++ DFC.App.DiscoverSkillsCareers.TestSuite/EnvironmentSettings.cs
@@ -11,12 +11,12 @@ namespace DFC.App.DiscoverSkillsCareers.TestSuite
 
         public IConfiguration GetConfiguration() { return Configuration; }
 
-        public string CosmosDbConnectionAccessKey => Configuration["Configuration:CosmosDbConnections:DysacContent:AccessKey"];
-        public string CosmosDbConnectionEndpoint => Configuration["Configuration:CosmosDbConnections:DysacContent:EndpointUrl"];
-        public string CosmosDbConnectionDatabaseId => Configuration["Configuration:CosmosDbConnections:DysacContent:DatabaseId"];
-        public string DysacContentCollectionId => Configuration["Configuration:CosmosDbConnections:DysacContent:CollectionId"];
-        public string DysacAssessmentCollectionId => Configuration["Configuration:CosmosDbConnections:DysacAssessment:CollectionId"];
-        public string SessionStateCollectionId => Configuration["Configuration:CosmosDbConnections:SessionState:CollectionId"];
+        //public string CosmosDbConnectionAccessKey => Configuration["Configuration:CosmosDbConnections:DysacContent:AccessKey"];
+        //public string CosmosDbConnectionEndpoint => Configuration["Configuration:CosmosDbConnections:DysacContent:EndpointUrl"];
+        //public string CosmosDbConnectionDatabaseId => Configuration["Configuration:CosmosDbConnections:DysacContent:DatabaseId"];
+        //public string DysacContentCollectionId => Configuration["Configuration:CosmosDbConnections:DysacContent:CollectionId"];
+        //public string DysacAssessmentCollectionId => Configuration["Configuration:CosmosDbConnections:DysacAssessment:CollectionId"];
+        //public string SessionStateCollectionId => Configuration["Configuration:CosmosDbConnections:SessionState:CollectionId"];
         public string DYSACApiBaseUrl => Configuration["DYSACApi:BaseUrl"];
         public bool PipelineRun => Environment.GetEnvironmentVariable("SYSTEM_TEAMFOUNDATIONCOLLECTIONURI") == "https://sfa-gov-uk.visualstudio.com/";
     }
