5420476e79c8b6980783da24e8c836f51ebdfc5f
vicsdet <30327964+vaibe@users.noreply.github.com>
2022-04-04 17:56:07 +0100

    TC09 - Phone number field validation

---
+++ DFC.App.DiscoverSkillsCareers.TestSuite/PageObjects/ReturnToAssessmentPage.cs
@@ -0,0 +1,50 @@
+﻿using DFC.App.DiscoverSkillsCareers.TestSuite.Extensions;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.DiscoverSkillsCareers.TestSuite.PageObjects
+{
+    class ReturnToAssessmentPage
+    {
+        private ScenarioContext _scenarioContext;
+        public ReturnToAssessmentPage(ScenarioContext context)
+        {
+            _scenarioContext = context;
+        }
+
+        IWebElement optionReferenceCode => _scenarioContext.GetWebDriver().FindElement(By.XPath(".//div[@class='govuk-radios__item']/label[@for='SelectedOption-2']"));
+
+        IWebElement lnkReturnToAssessment => _scenarioContext.GetWebDriver().FindElement(By.LinkText("Return to assessment"));
+
+        IWebElement txtHeader => _scenarioContext.GetWebDriver().FindElement(By.CssSelector(".govuk-fieldset__heading"));
+
+        public void SelectReferenceCode()
+        {
+            WebDriverExtension.WaitElementToBeClickable(_scenarioContext.GetWebDriver(), By.XPath(".//div[@class='govuk-radios__item']/label[@for='SelectedOption-2']"));
+            optionReferenceCode.Click();
+        }
+
+        public void ClickReturnToAssessment()
+        {
+            WebDriverExtension.WaitElementToBeClickable(_scenarioContext.GetWebDriver(), By.ClassName("govuk-footer"));
+            lnkReturnToAssessment.Click();
+        }
+
+        public string GetElementAttribute(string elementLabel)
+        {
+            WebDriverExtension.WaitElementToBeClickable(_scenarioContext.GetWebDriver(), By.ClassName("govuk-footer"));
+
+            var attributeValue = _scenarioContext.GetWebDriver().FindElement(By.XPath("//label[contains(text(), '" + elementLabel + "')]//preceding::input")).GetAttribute("type");
+            return attributeValue;
+        }
+
+        public string GetHeaderText(string text)
+        {
+            WebDriverExtension.WaitElementToBeClickable(_scenarioContext.GetWebDriver(), By.ClassName("govuk-footer"));
+            return txtHeader.Text;
+        }
+    }
+}
--- DFC.App.DiscoverSkillsCareers.TestSuite/StepDefinitions/DYSACSteps.cs
+++ DFC.App.DiscoverSkillsCareers.TestSuite/StepDefinitions/DYSACSteps.cs
@@ -1,7 +1,11 @@
 ﻿using DFC.App.DiscoverSkillsCareers.TestSuite.Extensions;
+using DFC.App.DiscoverSkillsCareers.TestSuite.Helpers;
 using DFC.App.DiscoverSkillsCareers.TestSuite.PageObjects;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 using Xunit;
 
 namespace DFC.App.DiscoverSkillsCareers.TestSuite.StepDefinitions
@@ -11,11 +15,21 @@ namespace DFC.App.DiscoverSkillsCareers.TestSuite.StepDefinitions
     {
         private readonly ScenarioContext _scenarioContext;
         private readonly DysacPage _dysacPage;
+        private readonly ReturnToAssessmentPage _returnToAssessmentPage;
+        private readonly YourReferenceCodePage _yourReferenceCodePage;
+        private readonly CheckYourPhonePage _checkYourPhonePage;
+        private string theAnswerOption;
+        private string dateOnPage;
+        private string _phoneNumber;
+        private string _percentCompleted;
 
         public DYSACSteps(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
             _dysacPage = new DysacPage(_scenarioContext);
+            _returnToAssessmentPage = new ReturnToAssessmentPage(_scenarioContext);
+            _yourReferenceCodePage = new YourReferenceCodePage(_scenarioContext);
+            _checkYourPhonePage = new CheckYourPhonePage(_scenarioContext);
         }
 
         [Given]
@@ -24,6 +38,7 @@ namespace DFC.App.DiscoverSkillsCareers.TestSuite.StepDefinitions
             _scenarioContext.GetWebDriver().Navigate().GoToUrl(_scenarioContext.GetEnv().DYSACApiBaseUrl);
         }
 
+        [Given(@"I click on Assessment")]
         [When]
         public void WhenIClickOnAssessment()
         {
@@ -102,7 +117,6 @@ namespace DFC.App.DiscoverSkillsCareers.TestSuite.StepDefinitions
             _dysacPage.ClickContinueToSaveProgress();
         }
 
-
         [Then(@"The reference code is displayed")]
         public void ThenTheReferenceCodeIsDisplayed()
         {
@@ -121,5 +135,143 @@ namespace DFC.App.DiscoverSkillsCareers.TestSuite.StepDefinitions
             Assert.NotEmpty(_dysacPage.GetResultText());
         }
 
+        [When(@"I select ""(.*)"" option and answer questions to the end")]
+        public void WhenISelectOptionAndAnswerQuestionsToTheEnd(string answerOption)
+        {
+            theAnswerOption = answerOption;
+        }
+
+        [Then(@"the questions in turn have the following class attributes")]
+        public void ThenTheQuestionsInTurnHaveTheFollowingClassAttributes(Table table)
+        {
+            IEnumerable<LocatorAttributes> theAttributes = table.CreateSet<LocatorAttributes>().ToList();
+            
+            Assert.True(_dysacPage.VerifyProgressBar(theAttributes, theAnswerOption), "Progress bar not proceeding steadily");
+        }
+
+        [Given(@"I select the ""(.*)"" option")]
+        public void GivenISelectTheOption(string answerOption)
+        {
+            theAnswerOption = answerOption;
+        }
+
+        [Given(@"I proceed with answering questions up to ""(.*)"" percent")]
+        [Given(@"I proceed with answering questions up to (.*) percent")]
+        public void GivenIProceedWithAnsweringQuestionsUpToPercent(string percentComplete)
+        {
+            _percentCompleted = percentComplete;
+            _dysacPage.AnswerQuestions(theAnswerOption, percentComplete);
+        }
+
+        [When(@"I save progress")]
+        [Given(@"I save progress")]
+        public void GivenISaveProgress()
+        {
+            _dysacPage.ClickSaveProgress();
+        }
+
+        [When(@"I choose the ""(.*)"" option of returning to assessment")]
+        [Given(@"I choose the ""(.*)"" option of returning to assessment")]
+        public void GivenIChooseTheOptionOfReturningToAssessment(string assessmentReturnOption)
+        {
+            _returnToAssessmentPage.SelectReferenceCode();
+            _dysacPage.ClickContinueToSaveProgress();
+        }
+
+        [When(@"I make a note of the reference code")]
+        [Given(@"I make a note of the reference code")]
+        public void GivenIMakeANoteOfTheReferenceCode()
+        {
+            _yourReferenceCodePage.GetReferenceCode();
+        }
+
+        [When(@"I use the reference code to return to my assessment from the Dysac home page")]
+        public void WhenIUseTheReferenceCodeToReturnToMyAssessmentFromTheDysacHomePage()
+        {
+            _dysacPage.EnterYourReference(_yourReferenceCodePage.ReferenceCode);
+            _dysacPage.ClickContinueToSaveProgress();
+        }
+
+        [Then(@"I am at the question where I left off")]
+        public void ThenIAmAtTheQuestionWhereILeftOff()
+        {
+            NUnit.Framework.Assert.AreEqual(_percentCompleted + "%", _dysacPage.GetPercentageComplete(), "Reference code did not go to the correct percentage completed.");
+        }
+
+        [When(@"I view the date on the resultant page")]
+        public void WhenIViewTheDateOnTheResultantPage()
+        {
+            dateOnPage = _yourReferenceCodePage.GetDate();
+        }
+
+        [Then(@"the date is todays date")]
+        public void ThenTheDateIsTodaysDate()
+        {
+            NUnit.Framework.Assert.AreEqual(dateOnPage, DateTime.Now.ToString("d MMMM yyy"), "The date is incorrect.");
+        }
+
+        [When(@"I click the Return to assessment link")]
+        public void WhenIClickTheReturnToAssessmentLink()
+        {
+            _returnToAssessmentPage.ClickReturnToAssessment();
+        }
+
+        [When(@"I supply phone number ""(.*)""")]
+        public void WhenISupplyPhoneNumber(string phoneNumber)
+        {
+            _phoneNumber = phoneNumber;
+            _yourReferenceCodePage.EnterPhoneNumber(phoneNumber);
+            _dysacPage.ClickContinueToSaveProgress();
+        }
+
+        [Then(@"the phone number appears on the Check your phone page")]
+        public void ThenThePhoneNumberAppearsOnTheCheckYourPhonePage()
+        {
+            NUnit.Framework.Assert.AreEqual(_phoneNumber, _checkYourPhonePage.GetPhoneNumber().Replace("A text message has been sent to", string.Empty).Trim(), "The phone number is incorrect.");
+        }
+
+        [When(@"I click the Return to assessment button")]
+        public void WhenIClickTheReturnToAssessmentButton()
+        {
+            _checkYourPhonePage.ReturnToAssessment();
+        }
+
+        [When(@"I go Back and I click the Back to start link")]
+        public void WhenIGoBackAndIClickTheBackToStartLink()
+        {
+            _dysacPage.GoBack();
+            _checkYourPhonePage.ClickBackToStart();
+        }
+
+        [Then(@"I am navigate to the Dysac home page")]
+        public void ThenIAmNavigateToTheDysacHomePage()
+        {
+            NUnit.Framework.Assert.AreEqual(_scenarioContext.GetEnv().DYSACApiBaseUrl, _dysacPage.GetUrl(), "Link did not navigate to the home page");
+        }
+
+        [Then(@"the ""(.*)"" radio button is present on the resultant page")]
+        public void ThenTheRadioButtonIsPresentOnTheResultantPage(string radioButton)
+        {
+            NUnit.Framework.Assert.AreEqual("radio", _returnToAssessmentPage.GetElementAttribute(radioButton), "Radio button not present");
+        }
+
+        [Then(@"validation messages are displayed")]
+        public void ThenValidationMessagesAreDisplayed()
+        {
+            NUnit.Framework.Assert.IsTrue(_yourReferenceCodePage.ValidationBoxPresent(), "The validation box is not present.");
+            NUnit.Framework.Assert.AreEqual("Enter a phone number", _yourReferenceCodePage.PhoneValidationMsg(), "The validation message was not displayed.");
+        }
+
+        [When(@"I click Back")]
+        public void WhenIClickBack()
+        {
+            _yourReferenceCodePage.ClickBackLink();
+        }
+
+        [Then(@"I am navigated to the ""(.*)"" page")]
+        public void ThenIAmNavigatedToThePage(string pageText)
+        {
+            NUnit.Framework.Assert.AreEqual(pageText, _returnToAssessmentPage.GetHeaderText(pageText), "Navigation has not been to the correct page.");
+        }
     }
 }
