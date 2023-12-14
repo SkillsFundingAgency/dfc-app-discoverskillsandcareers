89e8932205029e036a8e66a450fecb1b4a8d57d7
Lian-Athofer1 <150462854+Lian-Athofer1@users.noreply.github.com>
2023-12-14 12:30:15 +0000

    Created the view models for the breadcrumbs

---
+++ DFC.App.DiscoverSkillsCareers/ViewModels/BreadcrumbViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.App.SkillsHealthCheck.ViewModels
+{
+    [ExcludeFromCodeCoverage]
+    public class BreadcrumbViewModel
+    {
+        public List<BreadcrumbItemViewModel>? Breadcrumbs { get; set; }
+    }
+}
