f6d52336b84bf080e47e1ece500360da477526b6
Lian-Athofer1 <150462854+Lian-Athofer1@users.noreply.github.com>
2023-12-19 17:14:12 +0000

    Changes

--- DFC.App.DiscoverSkillsCareers/ViewModels/BreadcrumbViewModel.cs
+++ DFC.App.DiscoverSkillsCareers/ViewModels/BreadcrumbViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
-namespace DFC.App.SkillsHealthCheck.ViewModels
+namespace DFC.App.DiscoverSkillsCareers.ViewModels
 {
-    [ExcludeFromCodeCoverage]
     public class BreadcrumbViewModel
     {
         public List<BreadcrumbItemViewModel>? Breadcrumbs { get; set; }
