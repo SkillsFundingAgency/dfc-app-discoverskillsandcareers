01248a2bcbf3f786035e68d097110a33c19d5097
hnandra <32364605+hnandra@users.noreply.github.com>
2024-02-13 16:35:36 +0000

    Implemented GraphQl for JobProfileOverview

---
+++ DFC.App.DiscoverSkillsCareers/ViewModels/JobProfileViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DFC.App.DiscoverSkillsCareers.ViewModels
+{
+    public class JobProfileViewModel
+    {
+        public string DisplayText { get; set; }
+
+        public string Overview { get; set; }
+
+        public string SalaryStarterPerYear { get; set; }
+
+        public string SalaryExperiencedPerYear { get; set; }
+
+        public string UrlName { get; set; }
+
+        public string MaximumHours { get; set; }
+
+        public string MinimumHours { get; set; }
+
+        public string WorkingPattern { get; set; }
+
+        public string WorkingHoursDetails { get; set; }
+
+        public string WorkingPatternDetails { get; set; }
+    }
+}
