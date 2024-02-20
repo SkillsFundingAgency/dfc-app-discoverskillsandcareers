b2326e9af6afebabc2052f7181109dbbc0c40988
hnandra <32364605+hnandra@users.noreply.github.com>
2024-02-20 12:24:48 +0000

    Added unit test for the GraphQlService

---
+++ DFC.App.DiscoverSkillsCareers.UnitTests/GraphQl/GraphQlServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using DFC.App.DiscoverSkillsCareers.GraphQl;
+using DFC.App.DiscoverSkillsCareers.MappingProfiles;
+using DFC.App.DiscoverSkillsCareers.ViewModels;
+using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
+using DFC.Common.SharedContent.Pkg.Netcore.Model.Response;
+using FakeItEasy;
+using FluentAssertions;
+using Razor.Templating.Core;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.App.DiscoverSkillsCareers.UnitTests.GraphQl
+{
+    public class GraphQlServiceTests
+    {
+        [Fact]
+        public async Task GetJobProfileAsyncShouldReturnJobProfile()
+        {
+            // Arrange
+            var mapper = new MapperConfiguration(cfg => cfg.AddProfile(new ResultProfileOverviewsProfile())).CreateMapper();
+            var fakeSharedContentRedisInterface = A.Fake<ISharedContentRedisInterface>();
+            var fakeRazorTemplateEngine = A.Fake<IRazorTemplateEngine>();
+
+            A.CallTo(() => fakeSharedContentRedisInterface.GetDataAsync<JobProfileOverviewResponse>(A<string>.Ignored)).Returns(A.Dummy<JobProfileOverviewResponse>());
+
+            var service = new GraphQlService(fakeSharedContentRedisInterface, fakeRazorTemplateEngine, mapper);
+
+            // Act
+            var result = await service.GetJobProfileAsync("test");
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<JobProfileViewModel>();
+        }
+    }
+}
