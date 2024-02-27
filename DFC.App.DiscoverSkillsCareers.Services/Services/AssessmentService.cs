b688a80e8a26b7e25eb5c0c3d6be5fd53ffdea94
Zhaoming LU <Zhaoming.LU@EDUCATION.GOV.UK>
2024-02-27 19:59:34 +0000

    Remove white space

--- DFC.App.DiscoverSkillsCareers.Services/Services/AssessmentService.cs
+++ DFC.App.DiscoverSkillsCareers.Services/Services/AssessmentService.cs
@@ -252,12 +252,11 @@ namespace DFC.App.DiscoverSkillsCareers.Services.Services
 
         public async Task<DysacAssessment?> GetAssessment(string sessionId, bool throwErrorWhenNotFound)
         {
-
             if (accessor.HttpContext != null && accessor.HttpContext.Items.ContainsKey(HttpContextAssessmentKey))
             {
               return (DysacAssessment?)accessor.HttpContext.Items[HttpContextAssessmentKey];
             }
-            
+
             var assessment = await documentStore.GetAssessmentAsync(sessionId)
                 .ConfigureAwait(false);
 
