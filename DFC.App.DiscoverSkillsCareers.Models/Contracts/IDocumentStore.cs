92f62f42588e4c10846b47418e75e9841e79eaa9
hnandra <32364605+hnandra@users.noreply.github.com>
2024-04-15 17:09:23 +0100

    Removed content from cosmosdbservice

--- DFC.App.DiscoverSkillsCareers.Models/Contracts/IDocumentStore.cs
+++ DFC.App.DiscoverSkillsCareers.Models/Contracts/IDocumentStore.cs
@@ -1,7 +1,4 @@
 using DFC.App.DiscoverSkillsCareers.Models.Assessment;
-using System;
-using System.Collections.Generic;
-using System.Net;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
@@ -9,21 +6,8 @@ namespace DFC.App.DiscoverSkillsCareers.Models.Contracts
 {
     public interface IDocumentStore
     {
-        public Task<T?> GetContentByIdAsync<T>(Guid id, string partitionKey, [CallerMemberName] string callerMemberName = "")
-            where T : class;
-
-        public Task<List<T>> GetAllContentAsync<T>(string partitionKey, [CallerMemberName] string callerMemberName = "")
-            where T : class;
-
         public Task<DysacAssessment?> GetAssessmentAsync(string sessionId, [CallerMemberName] string callerMemberName = "");
 
         public Task UpdateAssessmentAsync(DysacAssessment assessment, [CallerMemberName] string callerMemberName = "");
-
-        public Task<HttpStatusCode> CreateContentAsync<T>(T content, [CallerMemberName] string callerMemberName = "");
-
-        public Task<HttpStatusCode> UpdateContentAsync<T>(T content, [CallerMemberName] string callerMemberName = "");
-
-        public Task<bool> DeleteContentAsync<T>(Guid id, string partitionKey, [CallerMemberName] string callerMemberName = "")
-            where T : class;
     }
 }
