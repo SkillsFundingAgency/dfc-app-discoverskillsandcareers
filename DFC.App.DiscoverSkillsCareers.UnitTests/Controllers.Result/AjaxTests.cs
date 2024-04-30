a8cf2ce23652b0ea9e20c06e116624891adce635
Lovre-Schonauer <116816530+Lovre-Schonauer@users.noreply.github.com>
2024-04-30 15:45:54 +0100

    Finished rebasing with release 57.4

--- DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.Result/AjaxTests.cs
+++ DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.Result/AjaxTests.cs
@@ -2,7 +2,6 @@
 using DFC.App.DiscoverSkillsCareers.Controllers;
 using DFC.App.DiscoverSkillsCareers.Models.Contracts;
 using DFC.App.DiscoverSkillsCareers.Services.Contracts;
-using DFC.App.DiscoverSkillsCareers.Services.Models;
 using DFC.Compui.Cosmos.Contracts;
 using DFC.Content.Pkg.Netcore.Data.Models.ClientOptions;
 using DFC.Logger.AppInsights.Contracts;
@@ -20,37 +19,34 @@ using System.Threading.Tasks;
 using Xunit;
 using Microsoft.Extensions.Configuration;
 using Moq;
+using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
+using Razor.Templating.Core;
 
 namespace DFC.App.DiscoverSkillsCareers.UnitTests.Controllers.Result
 {
     public class AjaxTests
     {
-        private readonly ResultsController controller;
+        private readonly ILogService logService;
         private readonly IMapper mapper;
         private readonly ISessionService sessionService;
         private readonly IAssessmentService assessmentService;
         private readonly IResultsService resultsService;
-        private readonly ILogService logService;
-        private readonly IDocumentStore documentStore;
-        private readonly IDocumentService<StaticContentItemModel> staticContentDocumentService;
-        private readonly CmsApiClientOptions cmsApiClientOptions;
+        private readonly ISharedContentRedisInterface sharedContentRedisInterface;
+        private readonly IRazorTemplateEngine razorTemplateEngine;
+        private readonly IConfiguration configuration;
+        private readonly ResultsController controller;
 
         public AjaxTests()
         {
+            logService = A.Fake<ILogService>();
             mapper = A.Fake<IMapper>();
             sessionService = A.Fake<ISessionService>();
             assessmentService = A.Fake<IAssessmentService>();
             resultsService = A.Fake<IResultsService>();
-            logService = A.Fake<ILogService>();
-            documentStore = A.Fake<IDocumentStore>();
-            var fakeMemoryCache = A.Fake<IMemoryCache>();
-            staticContentDocumentService = A.Fake<IDocumentService<StaticContentItemModel>>();
-            cmsApiClientOptions = new CmsApiClientOptions
-            {
-                ContentIds = Guid.NewGuid().ToString(),
-            };
-
-            controller = new ResultsController(logService, mapper, sessionService, resultsService, assessmentService, documentStore, fakeMemoryCache, staticContentDocumentService, cmsApiClientOptions);
+            sharedContentRedisInterface = A.Fake<ISharedContentRedisInterface>();
+            configuration = A.Fake<IConfiguration>();
+            razorTemplateEngine = A.Fake<IRazorTemplateEngine>();
+            controller = new ResultsController(logService, mapper, sessionService, resultsService, assessmentService, sharedContentRedisInterface, razorTemplateEngine, configuration);
         }
 
         //[Fact]
