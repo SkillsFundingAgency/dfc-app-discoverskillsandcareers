98adc429461f7f706a3d7536ca96a609f6969bf9
Lovre-Schonauer <116816530+Lovre-Schonauer@users.noreply.github.com>
2024-04-30 16:16:19 +0100

    Fixed unit test

--- DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.Result/AjaxTests.cs
+++ DFC.App.DiscoverSkillsCareers.UnitTests/Controllers.Result/AjaxTests.cs
@@ -21,46 +21,43 @@ using Microsoft.Extensions.Configuration;
 using Moq;
 using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
 using Razor.Templating.Core;
+using DFC.App.DiscoverSkillsCareers.Models.Assessment;
 
 namespace DFC.App.DiscoverSkillsCareers.UnitTests.Controllers.Result
 {
     public class AjaxTests
     {
-        private readonly ILogService logService;
-        private readonly IMapper mapper;
-        private readonly ISessionService sessionService;
-        private readonly IAssessmentService assessmentService;
-        private readonly IResultsService resultsService;
-        private readonly ISharedContentRedisInterface sharedContentRedisInterface;
-        private readonly IRazorTemplateEngine razorTemplateEngine;
-        private readonly IConfiguration configuration;
+        private readonly ILogService FakeLogService;
+        private readonly IMapper FakeMapper;
+        private readonly ISessionService FakeSessionService;
+        private readonly IAssessmentService FakeAssessmentService;
+        private readonly IResultsService FakeResultsService;
+        private readonly ISharedContentRedisInterface FakeSharedContentRedisInterface;
+        private readonly IRazorTemplateEngine FakeRazorTemplateEngine;
+        private readonly IConfiguration FakeConfiguration;
         private readonly ResultsController controller;
 
         public AjaxTests()
         {
-            logService = A.Fake<ILogService>();
-            mapper = A.Fake<IMapper>();
-            sessionService = A.Fake<ISessionService>();
-            assessmentService = A.Fake<IAssessmentService>();
-            resultsService = A.Fake<IResultsService>();
-            sharedContentRedisInterface = A.Fake<ISharedContentRedisInterface>();
-            configuration = A.Fake<IConfiguration>();
-            razorTemplateEngine = A.Fake<IRazorTemplateEngine>();
-            controller = new ResultsController(logService, mapper, sessionService, resultsService, assessmentService, sharedContentRedisInterface, razorTemplateEngine, configuration);
+            FakeLogService = A.Fake<ILogService>();
+            FakeMapper = A.Fake<IMapper>();
+            FakeSessionService = A.Fake<ISessionService>();
+            FakeAssessmentService = A.Fake<IAssessmentService>();
+            FakeResultsService = A.Fake<IResultsService>();
+            FakeSharedContentRedisInterface = A.Fake<ISharedContentRedisInterface>();
+            FakeConfiguration = A.Fake<IConfiguration>();
+            FakeRazorTemplateEngine = A.Fake<IRazorTemplateEngine>();
+            controller = new ResultsController(FakeLogService, FakeMapper, FakeSessionService, FakeResultsService, FakeAssessmentService, FakeSharedContentRedisInterface, FakeRazorTemplateEngine, FakeConfiguration);
         }
 
-        //[Fact]
-        //public async Task AjaxChangedCallsUpdateJobCategoryCountsSuccessfully()
-        //{
-        //    //arranage
-        //    var sessionId = await sessionService.GetSessionId().ConfigureAwait(false);
-        //    var assessment = await assessmentService.GetAssessment(sessionId).ConfigureAwait(false);
+        [Fact]
+        public async Task AjaxChangedCallsUpdateJobCategoryCountsSuccessfully()
+        {
+            //act
+            await controller.AjaxChanged().ConfigureAwait(false);
 
-        //    //act
-        //    await controller.AjaxChanged().ConfigureAwait(false);
-
-        //    //assert
-        //    A.CallTo(() => resultsService.UpdateJobCategoryCounts(assessment).ConfigureAwait(false)).MustHaveHappenedOnceExactly();
-        //}
+            //assert
+            A.CallTo(() => FakeResultsService.UpdateJobCategoryCounts(A<DysacAssessment>.Ignored)).MustHaveHappenedOnceExactly();
+        }
     }
 }
