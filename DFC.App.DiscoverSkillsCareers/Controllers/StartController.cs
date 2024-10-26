0e67b42beb0c431f3c8f4843da923a301ec28040
skumardfe <Santhosh.KUMAR@EDUCATION.GOV.UK>
2024-10-26 14:47:22 +0100

    Implemented dysac start page

---
+++ DFC.App.DiscoverSkillsCareers/Controllers/StartController.cs
@@ -0,0 +1,92 @@
+﻿using DFC.App.DiscoverSkillsCareers.Core.Constants;
+using DFC.App.DiscoverSkillsCareers.Models.Assessment;
+using DFC.App.DiscoverSkillsCareers.Services.Contracts;
+using DFC.App.DiscoverSkillsCareers.Services.Helpers;
+using DFC.App.DiscoverSkillsCareers.ViewModels;
+using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
+using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems;
+using DFC.Common.SharedContent.Pkg.Netcore.Model.ContentItems.SharedHtml;
+using DFC.Logger.AppInsights.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Constants = DFC.Common.SharedContent.Pkg.Netcore.Constant.ApplicationKeys;
+
+namespace DFC.App.DiscoverSkillsCareers.Controllers
+{
+    public class StartController : BaseController
+    {
+        private const string ExpiryAppSettings = "Cms:Expiry";
+        private readonly IAssessmentService assessmentService;
+        private readonly ISharedContentRedisInterface sharedContentRedisInterface;
+        private readonly IConfiguration configuration;
+        private string status;
+        private double expiryInHours = 4;
+        private readonly ILogService logService;
+
+        public StartController(
+            ISessionService sessionService,
+            IAssessmentService assessmentService,
+            ISharedContentRedisInterface sharedContentRedisInterface,
+            IConfiguration configuration,
+            ILogService logService)
+            : base(sessionService)
+        {
+            this.assessmentService = assessmentService;
+            this.sharedContentRedisInterface = sharedContentRedisInterface;
+            this.configuration = configuration;
+            this.logService = logService;
+
+            status = configuration?.GetSection("contentMode:contentMode").Get<string>();
+
+            if (string.IsNullOrEmpty(status))
+            {
+                status = "PUBLISHED";
+            }
+
+            if (this.configuration != null)
+            {
+                string expiryAppString = this.configuration.GetSection(ExpiryAppSettings).Get<string>();
+                if (double.TryParse(expiryAppString, out var expiryAppStringParseResult))
+                {
+                    expiryInHours = expiryAppStringParseResult;
+                }
+            }
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var hasSessionId = await HasSessionId().ConfigureAwait(false);
+            if (!hasSessionId)
+            {
+                return RedirectToRoot();
+            }
+
+            var startViewModel = await GetAssessmentViewModel().ConfigureAwait(false);
+
+            var sharedhtml = sharedContentRedisInterface.GetDataAsyncWithExpiry<SharedHtml>(Constants.SpeakToAnAdviserSharedContent, status, expiryInHours).Result.Html;
+            startViewModel.SharedContent = sharedhtml;
+
+            return View(startViewModel);
+        }
+
+
+        private async Task<StartViewModel> GetAssessmentViewModel()
+        {
+            var getAssessmentResponse = await GetAssessment().ConfigureAwait(false);
+
+            var result = new StartViewModel
+            {
+                ReferenceCode = SessionHelper.FormatSessionId(getAssessmentResponse.ReferenceCode!),
+                AssessmentStarted = getAssessmentResponse.StartedDt.ToString(DateTimeFormat.Standard),
+            };
+
+            return result;
+        }
+
+        private async Task<GetAssessmentResponse> GetAssessment()
+        {
+            return await assessmentService.GetAssessment().ConfigureAwait(false);
+        }
+    }
+}
