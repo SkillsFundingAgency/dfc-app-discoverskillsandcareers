d55604ab379ccd0489945c685ee4ecee7117b290
skumardfe <Santhosh.KUMAR@EDUCATION.GOV.UK>
2024-11-04 17:13:45 +0000

    refactired code

---
+++ DFC.App.DiscoverSkillsCareers.Services.UnitTests/ServiceTests/CommonServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using DFC.App.DiscoverSkillsCareers.Models.Contracts;
+using DFC.App.DiscoverSkillsCareers.Services.Contracts;
+using DFC.App.DiscoverSkillsCareers.Services.Services;
+using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
+using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace DFC.App.DiscoverSkillsCareers.Services.UnitTests.ServiceTests
+{
+    public class CommonServiceTests
+    {
+        private readonly ICommonService commonService;
+        private readonly ISessionService sessionService;
+        private readonly INotificationService notificationService;
+
+        public CommonServiceTests()
+        {
+            
+            sessionService = A.Fake<ISessionService>();
+            notificationService = A.Fake<INotificationService>();
+
+            commonService = new CommonService(            
+            sessionService,
+            notificationService);
+        }
+
+        [Fact]
+        public void AssessmentServiceSendSmsSendsSms()
+        {
+            // Arrange
+            // Act
+            commonService.SendSms("Testdomain.com", "07867511111");
+
+            // Assert
+            A.CallTo(() => notificationService.SendSms(A<string>.Ignored, A<string>.Ignored, A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public void AssessmentServiceSendEmailSendsEmail()
+        {
+            // Arrange
+            // Act
+            commonService.SendEmail("Testdomain.com", "test1@gmail.com");
+
+            // Assert
+            A.CallTo(() => notificationService.SendEmail(A<string>.Ignored, A<string>.Ignored, A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
--- DFC.App.DiscoverSkillsCareers.Services/Services/CommonService.cs
+++ DFC.App.DiscoverSkillsCareers.Services/Services/CommonService.cs
@@ -1,66 +1,21 @@
-﻿using AutoMapper;
-using DFC.App.DiscoverSkillsCareers.Models.Assessment;
-using DFC.App.DiscoverSkillsCareers.Models.Contracts;
+﻿using DFC.App.DiscoverSkillsCareers.Models.Assessment;
 using DFC.App.DiscoverSkillsCareers.Services.Contracts;
 using DFC.App.DiscoverSkillsCareers.Services.Helpers;
-using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using System;
 using System.Threading.Tasks;
 
 namespace DFC.App.DiscoverSkillsCareers.Services.Services
 {
     public class CommonService : ICommonService
     {
-        private const string ExpiryAppSettings = "Cms:Expiry";
-        private const string HttpContextAssessmentKey = "DysacAssessment";
-
-        private readonly ISessionIdToCodeConverter sessionIdToCodeConverter;
         private readonly ISessionService sessionService;
-        private readonly IDocumentStore documentStore;
-        private readonly IMapper mapper;
         private readonly INotificationService notificationService;
-        private readonly IHttpContextAccessor accessor;
-        private readonly ISharedContentRedisInterface sharedContentRedisInterface;
-        private readonly IConfiguration configuration;
-        private string status;
-        private double expiryInHours = 4;
 
         public CommonService(
-            ISessionIdToCodeConverter sessionIdToCodeConverter,
             ISessionService sessionService,
-            IDocumentStore documentStore,
-            IMapper mapper,
-            INotificationService notificationService,
-            IHttpContextAccessor accessor,
-            ISharedContentRedisInterface sharedContentRedisInterface,
-            IConfiguration configuration)
+            INotificationService notificationService)
         {
-            this.sessionIdToCodeConverter = sessionIdToCodeConverter;
             this.sessionService = sessionService;
-            this.documentStore = documentStore ?? throw new ArgumentNullException(nameof(documentStore));
-            this.mapper = mapper;
             this.notificationService = notificationService;
-            this.accessor = accessor;
-            this.sharedContentRedisInterface = sharedContentRedisInterface;
-            this.configuration = configuration;
-
-            status = configuration?.GetSection("contentMode:contentMode").Get<string>();
-
-            if (string.IsNullOrEmpty(status))
-            {
-                status = "PUBLISHED";
-            }
-
-            if (this.configuration != null)
-            {
-                string expiryAppString = this.configuration.GetSection(ExpiryAppSettings).Get<string>();
-                if (double.TryParse(expiryAppString, out var expiryAppStringParseResult))
-                {
-                    expiryInHours = expiryAppStringParseResult;
-                }
-            }
         }
 
         public async Task<SendEmailResponse> SendEmail(string domain, string emailAddress)
