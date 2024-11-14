629d1a0013ff5bc12d5d56f3f14c19322ab22bad
skumardfe <Santhosh.KUMAR@EDUCATION.GOV.UK>
2024-11-14 18:51:02 +0000

    fixed email format issues

--- DFC.App.DiscoverSkillsCareers/ViewModels/StartViewModel.cs
+++ DFC.App.DiscoverSkillsCareers/ViewModels/StartViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DFC.App.DiscoverSkillsCareers.Core.Enums;
+using DFC.App.DiscoverSkillsCareers.Services.DataAnnotations;
 using DFC.App.DiscoverSkillsCareers.Validation;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
@@ -17,10 +18,16 @@ namespace DFC.App.DiscoverSkillsCareers.ViewModels
         [Required(ErrorMessage = "Choose how to get your reference code sent to you")]
         public AssessmentReturnType? Contact { get; set; }
 
+        public bool IsEmail
+        {
+            get
+            {
+                return Contact == AssessmentReturnType.Email;
+            }
+        }
+
         [RequiredWhenSelectedAttribute(Values = new[] { nameof(AssessmentReturnType.Email) }, PropertyName = "Contact", ErrorMessage = "Enter a email")]
-        [RegularExpression(
-            @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$",
-            ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
+        [ValidateEmailAddressAttribute(Values = new[] { nameof(AssessmentReturnType.Email) }, PropertyName = "Contact", ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
         [DataType(DataType.EmailAddress, ErrorMessage = "Enter a email")]
         public string Email { get; set; }
 
@@ -30,6 +37,7 @@ namespace DFC.App.DiscoverSkillsCareers.ViewModels
             ErrorMessage = "Enter a mobile phone number, like 07700 900 982.")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "Enter a phone number")]
         public string PhoneNumber { get; set; }
+
         public DysacAction DysacAction { get; set; }
 
         public bool IsChecked(AssessmentReturnType answer)
